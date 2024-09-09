46c140ddd55ec411774fd8c47848a9bf4d6d1420
IdushaGaravi <idushagaravi@gmail.com>
2024-09-09 18:37:23 +0530

    add coments

--- Lab7/Exercise L7.E1/Exercise L7.E1.py
+++ Lab7/Exercise L7.E1/Exercise L7.E1.py
@@ -1,22 +1,13 @@
 # Exercise L7.E1
 
 get_input = input()     # get input file name from the user
-with open(get_input, 'r') as input_file:
-    data_list = input_file.read().split('\n')
+with open(get_input, 'r') as input_file:    # open input file, write and close it
+    data_list = input_file.read().split('\n')        # get data from input file
 
-D_max_list = []
-S_max_list = []
-length_list = []
+for i in range(len(data_list)):
+    length, Youngs_Modulus, Moment_Of_Inertia, load = list(data_list[i].split(' '))      # get length, Youngs_Modulus, Moment_Of_Inertia, load for corresponding beam   
 
-for line in data_list:
-    length, Youngs_Modulus, Moment_Of_Inertia, load = list(line.split(' '))
-    length_list.append(length)
+    D_max = ((float(load) * 1000) * ((float(length))**3))/(48 * (int(Youngs_Modulus) * (10**9)) * float(Moment_Of_Inertia))     # get the maximum deflection at the center of the beam
+    S_max = (((float(load) * 1000) * (float(length)))/(4 * float(Moment_Of_Inertia)))       # get the maximum bending stress at the center of the beam
 
-    D_max = ((float(load) * 1000) * ((float(length))**3))/(48 * (int(Youngs_Modulus) * (10**9)) * float(Moment_Of_Inertia))
-    S_max = (((float(load) * 1000) * (float(length)))/(4 * float(Moment_Of_Inertia)))
-
-    D_max_list.append('%0.6f'%D_max)
-    S_max_list.append('%0.2f'%S_max)
-
-for i in range(len(list(data_list))):
-    print(f'Beam {i+1}: Length: {length_list[i]} m, Max Deflection: {D_max_list[i]} m, Max Bending Stress: {S_max_list[i]} Pa')
+    print(f'Beam {i+1}: Length: {length} m, Max Deflection: {"%0.6f"%D_max} m, Max Bending Stress: {"%0.2f"%S_max} Pa')     # print the output in given form
